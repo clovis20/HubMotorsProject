9f65c50c8111b28796ef1fd7b10a43e63c57be46
clovis20 <clovis20@gmail.com>
2025-10-24 16:01:57 -0300

    Aplicando Refresh Após Alteração

---
+++ force-app/main/default/lwc/recordRefreshAction/recordRefreshAction.js
@@ -0,0 +1,7 @@
+// recordRefreshAction.js
+import { LightningElement } from 'lwc';
+
+export default class RecordRefreshAction extends LightningElement {
+    // Este componente não precisa de lógica JavaScript.
+    // Toda a funcionalidade de refresh está no arquivo de metadados XML.
+}
